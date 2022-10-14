a8d92302a3699b037e4437fe27de72797e531d65
Daniel de Mesquita <demesquita.daniel@gmail.com>
2022-10-14 14:08:09 -0300

    Cursos Let's Code

---
+++ CodingTank JAVA/HelloWorld/src/media.java
@@ -0,0 +1,11 @@
+public class media {
+    public static void main(String[] args) {
+        double nota1 = 9;
+        double nota2 = 7;
+        double nota3 = 5;
+
+        double resultado = ((nota1 *2) + (nota2 *3) + (nota3 *5)) / 10;
+
+        System.out.println("A média foi de: " + resultado);
+    }
+}
---
+++ CodingTank JAVA/HelloWorld/src/variaveis.java
@@ -0,0 +1,10 @@
+public class variaveis {
+    public static void main(String[] args) {
+        int idade = 41;
+        System.out.println(idade);
+        double altura = 1.79d;
+        System.out.println(altura);
+        final String CONSTANTE = "Esse valor é final e constante";
+
+    }
+}
