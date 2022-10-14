978a308eef4937727c4206fffb558e94bb55100a
demesquitadaniel <demesquita.daniel@gmail.com>
2022-10-14 16:30:03 -0300

    01

--- CodingTank JAVA/HelloWorld/src/ExercicioSalario.java
+++ CodingTank JAVA/HelloWorld/src/ExercicioSalario.java
@@ -11,12 +11,12 @@ public class ExercicioSalario {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Informe seu salário abaixo (somente números) e pressiona ENTER");
-        int salario = scanner.nextInt();
-        int ferias = salario * 1.3;
+        double salario = scanner.nextDouble();
+        double ferias = salario * 1.3;
         System.out.println("Em suas férias, você receberá R$ " + ferias);
-        int salarioAnual = (salario * 13) + ferias;
+        double salarioAnual = (salario * 13) + ferias;
         System.out.println("Seu salário anual, com décimo-terceiro e férias, é de R$ " + salarioAnual);
-        int descontoInss = salarioAnual * 0.3;
+        double descontoInss = salarioAnual * 0.3;
         System.out.println("Seu desconto de INSS é de R$ " + descontoInss);
         System.out.println("Portanto, você terá recebido, em todo o ano, a quantia líquida de R$ " + (salarioAnual - descontoInss));
     }
