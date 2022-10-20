2f0ce5f253e521cf18d87695bb92afd090e9e7f9
demesquitadaniel <demesquita.daniel@gmail.com>
2022-10-20 20:26:16 -0300

    Santander Coding Tank

--- CodingTank JAVA/HelloWorld/src/Array.java
+++ CodingTank JAVA/HelloWorld/src/Array.java
@@ -2,88 +2,79 @@ public class Array {
 
     public static void main(String[] args) {
 
-        int[] arrayInteiros = new int[3];
+//        int[] arrayInteiros = new int[3];
+//
+//        arrayInteiros[0] = 3;
+//        arrayInteiros[1] = 5;
+//        arrayInteiros[2] = 7;
+//
+//        System.out.println(arrayInteiros[0]);
+//        System.out.println(arrayInteiros[1]);
+//        System.out.println(arrayInteiros[2]);
+//
+//        String[] arrayStrings = new String[2];
+//
+//        System.out.println("========================================");
+//
+//        System.out.println(arrayStrings[0]);
+//        System.out.println(arrayStrings[1]);
+//
+//        System.out.println("========================================");
+//
+//        int[] arrayInteiros2 = {1,2,3,4,5,6};
+//        int[] arrayInteiros3 = new int[]{1,2,3};
+//
+//
+//        for(int i = 0; i < arrayInteiros2.length; i++) {
+//            if (arrayInteiros2[i] == 3) {
+//                System.out.println("O objeto está no índice " + i);
+//            }
+//        }
+//
+//        for(int inteiro : arrayInteiros2) {
+//            if (inteiro == 3)
+//                System.out.println("Existe o valor 3 no arrayInteiros2");
+//        }
 
-        arrayInteiros[0] = 3;
-        arrayInteiros[1] = 5;
-        arrayInteiros[2] = 9;
+        int[][] matriz = new int[3][];
+        matriz[0] = new int[5];
+        matriz[1] = new int[3];
 
-        System.out.println(arrayInteiros[0]);
-        System.out.println(arrayInteiros[1]);
-        System.out.println(arrayInteiros[2]);
+        int[][] arrayMultidimensaoInt = new int[2][2];
 
-        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        arrayMultidimensaoInt[0][0] = 3;
+        arrayMultidimensaoInt[0][1] = 5;
 
-        String[] arrayString = new String[2];
+        arrayMultidimensaoInt[1][0] = 10;
+        arrayMultidimensaoInt[1][1] = 12;
 
-        System.out.println(arrayString[0]);
-        System.out.println(arrayString[1]);
 
-        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+//        for(int i = 0; i < arrayMultidimensaoInt.length; i++) {
+//            for(int j = 0; j < arrayMultidimensaoInt[i].length; j++) {
+//                System.out.print(arrayMultidimensaoInt[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
 
-        int[] arrayInteiros2 = {1,2,3,4,5,6};
-        int[] arrayInteiros3 = new int[]{1,2,3};
-        System.out.println(arrayInteiros2[2]);
-        System.out.println(arrayInteiros2[0]);
 
-        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+//        int[][] matrizInteiros = new int[3][2];
+//        matrizInteiros[0][0] = 5;
+//        matrizInteiros[0][1] = 10;
+//
+//        matrizInteiros[1][0] = 20;
+//        matrizInteiros[1][1] = 30;
+//
+//        matrizInteiros[2][0] = 40;
+//        matrizInteiros[2][1] = 50;
 
-        for(int i = 0; i < arrayInteiros2.length; i++) {
-            System.out.println(arrayInteiros2[i]);
-        }
-
-        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-
-        for(int inteiro : arrayInteiros2) {
-            System.out.println(inteiro);
-        }
-
-        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-
-        int[][] arrayMultidimensao = new int[2][2];
-            System.out.println(arrayMultidimensao[0][0]); // 0
-
-            arrayMultidimensao[0][0] = 3;
-            arrayMultidimensao[0][1] = 5;
-            arrayMultidimensao[1][0] = 27;
-            arrayMultidimensao[1][1] = 99;
-
-        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-
-        System.out.println(arrayMultidimensao[0][0]); // 3
-        System.out.println(arrayMultidimensao[0][1]); // 5
-        System.out.println(arrayMultidimensao[1][0]); // 27
-        System.out.println(arrayMultidimensao[1][1]); // 99
-
-        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-
-        for(int i = 0; i < arrayMultidimensao.length; i++) {
-            for(int j = 0; j < arrayMultidimensao[i].length; j++) {
-                System.out.print(arrayMultidimensao[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-
-        int[][] matrizInteiros = new int[3][2];
-        matrizInteiros[0][0] = 5;
-        matrizInteiros[0][1] = 10;
-
-        matrizInteiros[1][0] = 25;
-        matrizInteiros[1][1] = 50;
-
-        matrizInteiros[2][0] = 100;
-        matrizInteiros[2][1] = 200;
+        int[][] matrizInteiros = {{5,10}, {20,30,50,40}, {40,50,1,2,3,4,5,7}};
 
         for(int i = 0; i < matrizInteiros.length; i++) {
             for(int j = 0; j < matrizInteiros[i].length; j++) {
                 System.out.print(matrizInteiros[i][j] + " ");
             }
             System.out.println();
-
         }
-
     }
 
 }
---
+++ CodingTank JAVA/HelloWorld/src/Aula05.java
@@ -0,0 +1,7 @@
+public class Aula05 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
