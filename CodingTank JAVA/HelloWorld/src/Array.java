030d388229faf3ee59bc18902660faee3b7d6f60
demesquitadaniel <demesquita.daniel@gmail.com>
2022-10-21 11:38:03 -0300

    Santander Coding Tank

--- CodingTank JAVA/HelloWorld/src/Array.java
+++ CodingTank JAVA/HelloWorld/src/Array.java
@@ -76,5 +76,4 @@ public class Array {
             System.out.println();
         }
     }
-
 }
--- CodingTank JAVA/HelloWorld/src/ExercicioTriangulo.java
+++ CodingTank JAVA/HelloWorld/src/ExercicioTriangulo.java
@@ -5,6 +5,16 @@ public class ExercicioTriangulo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+//        int numeroLinhas = scanner.nextInt();
+//
+//        for (int i = numeroLinhas; i > 0 ; i--){
+//            String caracter = "";
+//            for (int k = i; k > 0; k--) {
+//                caracter = caracter + "*";
+//            }
+//            System.out.println(caracter);
+//        }
+
         System.out.println("Digite abaixo a quantidade de linhas da parte superior do triângulo e pressione ENTER: ");
         int i = scanner.nextInt();
         String asterisco = "*";
@@ -13,6 +23,6 @@ public class ExercicioTriangulo {
             String linha = asterisco.repeat(i);
             System.out.println(linha);
             i--;
-            }
         }
     }
+}
